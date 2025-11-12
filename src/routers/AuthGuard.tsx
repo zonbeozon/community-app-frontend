a99b35cbb47b06fd1d1203addfa2a0d6dbbdcdce
진준호 <161407999+junhocode@users.noreply.github.com>
2025-11-12 23:52:05 +0900

    fixed: Stabilize AuthGuard to prevent redirect on refresh

--- src/routers/AuthGuard.tsx
+++ src/routers/AuthGuard.tsx
@@ -1,15 +1,32 @@
-import { Navigate, Outlet } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import { useAtomValue } from 'jotai';
+import { Navigate, Outlet } from 'react-router-dom';
 import { serverMemberAtom } from '@/atoms/authAtoms';
 import { ROUTE_PATH } from '@/constants/routePaths';
+import { Spinner } from "@/components/ui/spinner";
 
 const AuthGuard = () => {
   const serverMember = useAtomValue(serverMemberAtom);
 
+  const [isInitialized, setIsInitialized] = useState(false);
+
+  useEffect(() => {
+    setIsInitialized(true);
+  }, []); 
+
+  if (!isInitialized) {
+    return (
+      <div className="flex flex-col justify-center items-center gap-4 h-screen w-screen">
+        <Spinner />
+        인증 정보를 확인하는 중입니다...
+      </div>
+    );
+  }
+
   if (!serverMember) {
     return <Navigate to={ROUTE_PATH.root} replace />;
   }
-  
+
   return <Outlet />;
 };
 
