e102a39bba029f68826b7c45357592a4ec9ee683
진준호 <161407999+junhocode@users.noreply.github.com>
2025-11-12 19:17:29 +0900

    feat: Add spinner and styles for fallback

--- src/routers/router.tsx
+++ src/routers/router.tsx
@@ -1,20 +1,40 @@
-import React, { Suspense } from 'react';
-import { createBrowserRouter, createRoutesFromElements, Route, Outlet } from 'react-router-dom';
-import { ROUTE_PATH } from '@/constants/routePaths';
-import { MESSAGES } from '@/constants/messages';
-import Layout from '@/components/common/Layout/Layout';
-import AuthGuard from './AuthGuard';
+import React, { Suspense } from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Outlet,
+} from "react-router-dom";
+import { ROUTE_PATH } from "@/constants/routePaths";
+import { MESSAGES } from "@/constants/messages";
+import Layout from "@/components/common/Layout/Layout";
+import { Spinner } from "@/components/ui/spinner";
+import AuthGuard from "./AuthGuard";
 
-const Main = React.lazy(() => import('@/pages/Main/Main'));
-const ChannelContent = React.lazy(() => import('@/components/channel/ChannelContent/ChannelContent'));
-const PostList = React.lazy(() => import('@/components/post/PostList/PostList'));
-const PostDetail = React.lazy(() => import('@/components/post/PostDetail/PostDetail'));
-const Callback = React.lazy(() => import('@/pages/Callback/Callback'));
-const Landing = React.lazy(() => import('@/pages/Landing/Landing'));
-const ErrorPage = React.lazy(() => import('@/pages/Error/Error'));
+const Main = React.lazy(() => import("@/pages/Main/Main"));
+const ChannelContent = React.lazy(
+  () => import("@/components/channel/ChannelContent/ChannelContent")
+);
+const PostList = React.lazy(
+  () => import("@/components/post/PostList/PostList")
+);
+const PostDetail = React.lazy(
+  () => import("@/components/post/PostDetail/PostDetail")
+);
+const Callback = React.lazy(() => import("@/pages/Callback/Callback"));
+const Landing = React.lazy(() => import("@/pages/Landing/Landing"));
+const ErrorPage = React.lazy(() => import("@/pages/Error/Error"));
 
 const NoChannelSelected = () => (
-  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', width: '100%' }}>
+  <div
+    style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100%",
+      width: "100%",
+    }}
+  >
     <p>{MESSAGES.NO_SELECTED_CHANNEL}</p>
   </div>
 );
@@ -22,12 +42,28 @@ const NoChannelSelected = () => (
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route
-      element={<Suspense fallback={<div>페이지를 불러오는 중입니다...</div>}><Outlet /></Suspense>}
+      element={
+        <Suspense
+          fallback={
+            <div className="flex flex-col justify-center items-center gap-4 h-screen w-screen">
+              <Spinner />
+              페이지를 불러오는 중입니다...
+            </div>
+          }
+        >
+          <Outlet />
+        </Suspense>
+      }
       errorElement={<ErrorPage />}
     >
       <Route element={<Outlet />}>
-        <Route element={<Layout><Outlet /></Layout>}>
-          
+        <Route
+          element={
+            <Layout>
+              <Outlet />
+            </Layout>
+          }
+        >
           <Route path={ROUTE_PATH.root} element={<Landing />} />
 
           <Route element={<AuthGuard />}>
@@ -39,7 +75,6 @@ const router = createBrowserRouter(
               </Route>
             </Route>
           </Route>
-
         </Route>
       </Route>
 
