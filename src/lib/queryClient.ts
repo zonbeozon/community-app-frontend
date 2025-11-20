0fbaaa0d729a3f85943f4be11c69716ffe7b2c13
진준호 <161407999+junhocode@users.noreply.github.com>
2025-11-20 15:27:19 +0900

    feat: add defaultOptions for QueryClient

--- src/lib/queryClient.ts
+++ src/lib/queryClient.ts
@@ -1,3 +1,16 @@
 import { QueryClient } from '@tanstack/react-query';
+import axios from 'axios';
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: (failureCount, error) => {
+        if (axios.isAxiosError(error) && error.response?.status === 401) {
+          return false;
+        }
+        return failureCount < 2;
+      },
+      refetchOnWindowFocus: false, 
+    },
+  },
+});
