2b55ba2121c2156f8b95065d5dc757ba20264c84
진준호 <161407999+junhocode@users.noreply.github.com>
2025-11-28 17:34:44 +0900

    style: fix styles for ChannelBackButton

--- src/components/channel/ChannelBackButton/ChannelBackButton.tsx
+++ src/components/channel/ChannelBackButton/ChannelBackButton.tsx
@@ -1,18 +1,15 @@
-import { useNavigate } from "react-router-dom";
-import { ArrowLeft } from "lucide-react";
-import { Channel } from "@/types/channel.type";
-import { ROUTE_PATH } from "@/constants/routePaths";
-import * as S from "./ChannelBackButton.styles";
+import { ArrowLeft } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
+import { ROUTE_PATH } from '@/constants/routePaths';
+import type { Channel } from '@/types/channel.type';
+import * as S from './ChannelBackButton.styles';
 
-const ChannelBackButton = ({ channel }: { channel: Channel }) => {
+export const ChannelBackButton = ({ channel }: { channel: Channel }) => {
   const navigate = useNavigate();
 
   const handleGoBack = () => {
     if (channel) {
-      const channelPath = ROUTE_PATH.channelId.replace(
-        ":channelId",
-        String(channel.channelInfo.channelId)
-      );
+      const channelPath = ROUTE_PATH.channelId.replace(':channelId', String(channel.channelInfo.channelId));
       navigate(channelPath);
     } else {
       navigate(ROUTE_PATH.main);
@@ -20,14 +17,8 @@ const ChannelBackButton = ({ channel }: { channel: Channel }) => {
   };
 
   return (
-    <button
-      onClick={handleGoBack}
-      className={S.goBackButton}
-      aria-label="뒤로가기"
-    >
+    <button onClick={handleGoBack} className={S.goBackButton} aria-label="뒤로가기">
       <ArrowLeft size={20} />
     </button>
   );
 };
-
-export default ChannelBackButton;
