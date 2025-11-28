10067c395e1acad77837b7a37bc0edda16bc1a50
진준호 <161407999+junhocode@users.noreply.github.com>
2025-11-28 21:47:50 +0900

    style: fix style in ChannelProfileImage

--- src/components/channel/ChannelProfileImage/ChannelProfileImage.tsx
+++ src/components/channel/ChannelProfileImage/ChannelProfileImage.tsx
@@ -1,36 +1,24 @@
 import React from 'react';
-import { ChannelProfileImageProps } from '@/types/channel.type';  
-import * as S from "./ChannelProfileImage.styles";
+import type { ChannelProfileImageProps } from '@/types/channel.type';
+import * as S from './ChannelProfileImage.styles';
 
-const ChannelProfileImage = React.forwardRef<HTMLDivElement, ChannelProfileImageProps>(
+export const ChannelProfileImage = React.forwardRef<HTMLDivElement, ChannelProfileImageProps>(
   ({ channelInfo, size, className, ...props }, ref) => {
     const firstLetter = channelInfo.title[0].toUpperCase();
     const wrapperClass = size === 'sm' ? S.trigger.wrapper : undefined;
     const imageClass = size === 'sm' ? S.trigger.image : S.profileSection.image;
     const fallbackClass = size === 'sm' ? S.trigger.fallback : S.profileSection.fallback;
 
-    const imageElement = (
-      channelInfo.profile?.imageUrl ? (
-        <img
-          src={channelInfo.profile.imageUrl}
-          alt={`${channelInfo.title} 채널의 프로필`}
-          className={imageClass}
-        />
-      ) : (
-        <div className={fallbackClass}>{firstLetter}</div>
-      )
+    const imageElement = channelInfo.profile?.imageUrl ? (
+      <img src={channelInfo.profile.imageUrl} alt={`${channelInfo.title} 채널의 프로필`} className={imageClass} />
+    ) : (
+      <div className={fallbackClass}>{firstLetter}</div>
     );
 
     return (
-      <div
-        ref={ref}
-        {...props}
-        className={`${wrapperClass || ''} ${className || ''}`.trim()}
-      >
+      <div ref={ref} {...props} className={`${wrapperClass || ''} ${className || ''}`.trim()}>
         {imageElement}
       </div>
     );
-  }
+  },
 );
-
-export default ChannelProfileImage;
