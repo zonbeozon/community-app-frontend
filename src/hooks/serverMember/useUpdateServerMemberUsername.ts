7a14f27039ba6b2bc08310a4a270775c6f83547f
진준호 <161407999+junhocode@users.noreply.github.com>
2025-11-28 13:41:57 +0900

    feat: invalidate queries using username on usernamechange

--- src/hooks/serverMember/useUpdateServerMemberUsername.ts
+++ src/hooks/serverMember/useUpdateServerMemberUsername.ts
@@ -1,18 +1,22 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { toast } from 'sonner';
-import { updateServerMemberUserName } from '@/apis/http/serverMember.api';
-import { QUERY_KEYS } from '@/constants/queryKeys';
-import { SUCCESS_MESSAGES, SERVER_ERROR_MESSAGES } from '@/constants/messages';
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
+import { updateServerMemberUserName } from "@/apis/http/serverMember.api";
+import { QUERY_KEYS } from "@/constants/queryKeys";
+import { SUCCESS_MESSAGES, SERVER_ERROR_MESSAGES } from "@/constants/messages";
 
 const useUpdateServerMemberUsername = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: (payload: { username: string }) => updateServerMemberUserName(payload),
+    mutationFn: (payload: { username: string }) =>
+      updateServerMemberUserName(payload),
 
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: QUERY_KEYS.serverMember.me() });
-      
+      queryClient.invalidateQueries({ queryKey: QUERY_KEYS.posts.lists() });
+      queryClient.invalidateQueries({ queryKey: QUERY_KEYS.posts.details() });
+      queryClient.invalidateQueries({ queryKey: QUERY_KEYS.comments.lists() });
+
       toast.success(SUCCESS_MESSAGES.SERVERMEMBER_NAME_UPDATE_SUCCESS);
     },
 
