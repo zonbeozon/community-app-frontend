c8e9a38d4b690842b912c4a364051654013065b8
진준호 <161407999+junhocode@users.noreply.github.com>
2025-11-28 17:39:16 +0900

    refactor: fix style, dialog gets whole channel for props

--- src/components/channel/ChannelDropdown/ChannelDropdown.tsx
+++ src/components/channel/ChannelDropdown/ChannelDropdown.tsx
@@ -1,49 +1,23 @@
-import { Channel } from "@/types/channel.type";
-import { ActionDropdown } from "@/components/common/ActionDropdown/ActionDropdown";
-import ChannelUpdateDialog from "@/components/channel/ChannelUpdateDialog/ChannelUpdateDialog";
-import ChannelLeaveDialog from "@/components/channel/ChannelLeaveDialog/ChannelLeaveDialog";
-import ChannelDeleteDialog from "@/components/channel/ChannelDeleteDialog/ChannelDeleteDialog";
-import ChannelManageDialog from "@/components/channel/ChannelManageDialog/ChannelManageDialog";
-import { useChannelDropdown } from "@/hooks/channel/useChannelDropdown";
-import * as S from "./ChannelDropdown.styles";
+import { ChannelDeleteDialog } from '@/components/channel/ChannelDeleteDialog/ChannelDeleteDialog';
+import { ChannelLeaveDialog } from '@/components/channel/ChannelLeaveDialog/ChannelLeaveDialog';
+import { ChannelManageDialog } from '@/components/channel/ChannelManageDialog/ChannelManageDialog';
+import { ChannelUpdateDialog } from '@/components/channel/ChannelUpdateDialog/ChannelUpdateDialog';
+import { ActionDropdown } from '@/components/common/ActionDropdown/ActionDropdown';
+import { useChannelDropdown } from '@/hooks/channel/useChannelDropdown';
+import type { Channel } from '@/types/channel.type';
+import * as S from './ChannelDropdown.styles';
 
-const ChannelDropdown = ({ channel }: { channel: Channel }) => {
-  const {
-    dropdown,
-    manageDialog,
-    updateDialog,
-    deleteDialog,
-    leaveDialog,
-    actions,
-  } = useChannelDropdown(channel);
+export const ChannelDropdown = ({ channel }: { channel: Channel }) => {
+  const { dropdown, manageDialog, updateDialog, deleteDialog, leaveDialog, actions } = useChannelDropdown(channel);
 
   return (
     <>
-      <ActionDropdown
-        aria-label="채널 옵션"
-        {...dropdown}
-        actions={actions}
-        triggerClassName={S.dropdownButton}
-      /> 
-      
-      <ChannelManageDialog
-        {...manageDialog}
-        channelId={channel.channelInfo.channelId}
-      />
-      <ChannelUpdateDialog
-        {...updateDialog}
-        channelId={channel.channelInfo.channelId}
-      />
-      <ChannelDeleteDialog
-        {...deleteDialog}
-        channelId={channel.channelInfo.channelId}
-      />
-      <ChannelLeaveDialog
-        {...leaveDialog}
-        channelId={channel.channelInfo.channelId}
-      />
+      <ActionDropdown aria-label="채널 옵션" {...dropdown} actions={actions} triggerClassName={S.dropdownButton} />
+
+      <ChannelManageDialog {...manageDialog} channel={channel} />
+      <ChannelUpdateDialog {...updateDialog} channel={channel} />
+      <ChannelDeleteDialog {...deleteDialog} channel={channel} />
+      <ChannelLeaveDialog {...leaveDialog} channel={channel} />
     </>
   );
 };
-
-export default ChannelDropdown;
