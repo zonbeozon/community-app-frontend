21b43e19ba260111df5874b061df2ad369dc88af
진준호 <161407999+junhocode@users.noreply.github.com>
2025-12-02 16:20:22 +0900

    refactor: remove unncessary features of ChattingGroupHeader

--- src/components/chat/ChattingGroupHeader/ChattingGroupHeader.tsx
+++ src/components/chat/ChattingGroupHeader/ChattingGroupHeader.tsx
@@ -1,43 +1,12 @@
-import { useState } from 'react';
-import { ChannelBackButton } from '@/components/channel/ChannelBackButton/ChannelBackButton';
-import { ChannelDropdown } from '@/components/channel/ChannelDropdown/ChannelDropdown';
-import { ChannelInfoDialog } from '@/components/channel/ChannelInfoDialog/ChannelInfoDialog';
-import { ChannelJoinButton } from '@/components/channel/ChannelJoinButton/ChannelJoinButton';
-import { PostCreateDialog } from '@/components/post/PostCreateDialog/PostCreateDialog';
 import * as S from './ChattingGroupHeader.styles';
 import type { ChattingHeaderProps } from '@/types/chat.type';
 
-export const ChattingGroupHeader = ({ showBackButton, coinData }: ChattingHeaderProps) => {
-  const [isPostDialogOpen, setPostDialogOpen] = useState(false);
-
+export const ChattingGroupHeader = ({ coinData }: ChattingHeaderProps) => {
   return (
     <div className={S.wrapper}>
-      <div className={S.goBackButtonWrapper}>
-        {showBackButton && <ChannelBackButton channel={channelData} />}
-
-        <div className={S.image}>
-          <ChannelInfoDialog channel={channelData} />
-        </div>
-      </div>
-
       <div className={S.titleWrapper}>
-        <span className={S.name}>{channelData.channelInfo.title}</span>
+        <span className={S.name}>{coinData.name}</span>
       </div>
-
-      {isMember ? (
-        <>
-          <div className={S.button}>
-            {canCreatePost && <PostCreateDialog open={isPostDialogOpen} onOpenChange={setPostDialogOpen} />}
-          </div>
-          <div className={S.dropdownButton}>
-            <ChannelDropdown channel={channelData} />
-          </div>
-        </>
-      ) : (
-        <div className={S.button}>
-          <ChannelJoinButton channel={channelData} onJoinSuccess={() => {}} />
-        </div>
-      )}
     </div>
   );
 };
