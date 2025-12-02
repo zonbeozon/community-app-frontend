6f99572b328c6d87320c65be2843ec2941d1ddf9
진준호 <theworstvalentine@gmail.com>
2025-12-02 12:36:43 +0900

    feat: add ChattingGroupHeader

---
+++ src/components/chat/ChattingGroupHeader/ChattingGroupHeader.styles.ts
@@ -0,0 +1,18 @@
+export const wrapper = 'sticky top-0 z-20 w-full h-14 bg-white border-b flex items-center px-4';
+
+export const icon = 'w-13 h-13 rounded-full object-cover mr-3';
+
+export const titleWrapper = 'flex flex-col grow';
+
+export const name = 'text-md pl-3';
+
+export const image = 'justify-center items-center rounded-full object-cover';
+
+export const dropdownButton = `
+  h-8 w-8 flex items-center justify-center rounded-full
+  hover:bg-muted transition-colors
+`;
+
+export const button = 'pr-3';
+
+export const goBackButtonWrapper = 'flex items-center';
