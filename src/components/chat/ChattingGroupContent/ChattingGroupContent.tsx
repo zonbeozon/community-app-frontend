f9be37c97d8d0c7a183526dcf6c79e63dbc6378e
진준호 <161407999+junhocode@users.noreply.github.com>
2025-12-02 16:22:12 +0900

    feat: add datas for chart from useInfiniteKlinesQuery in ChattingGroupContent

--- src/components/chat/ChattingGroupContent/ChattingGroupContent.tsx
+++ src/components/chat/ChattingGroupContent/ChattingGroupContent.tsx
@@ -3,18 +3,39 @@ import { useCoinList } from '@/queries/useCoinList';
 import { Spinner } from '@/components/ui/spinner';
 import * as S from './ChattingGroupContent.styles';
 import { useMemo } from 'react';
+import { keepPreviousData } from "@tanstack/react-query";
+import { useInfiniteKlinesQuery } from '@/queries/useInfiniteKlineQueries';
 import { Chart } from '@/components/chart/Chart/Chart';
+import { ChattingGroupHeader } from '../ChattingGroupHeader/ChattingGroupHeader';
+import { ChatInput } from '../ChatInput/ChatInput';
 
 const ChattingGroupContent = () => {
   const { chattingGroupId } = useParams<{ chattingGroupId: string }>();
   
-  const { data: coins, isLoading } = useCoinList();
+  const { data: coins, isLoading: isCoinLoading } = useCoinList();
 
   const currentCoin = useMemo(() => {
     return coins?.find((c) => String(c.chattingGroupId) === chattingGroupId);
   }, [coins, chattingGroupId]);
 
-  if (isLoading) {
+  const chartParams = {
+    symbol: currentCoin?.symbol ? `${currentCoin.symbol}USDT` : "", 
+    interval: "1m", 
+    limit: 500,
+  };
+
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    isLoading: isChartLoading,
+  } = useInfiniteKlinesQuery(chartParams, {
+    placeholderData: keepPreviousData,
+    enabled: !!currentCoin?.symbol, 
+  });
+
+  if (isCoinLoading || isChartLoading) {
     return <Spinner />;
   }
 
@@ -22,18 +43,21 @@ const ChattingGroupContent = () => {
     return <div className={S.notFoundMessage}>❓ 존재하지 않는 종목 토론방입니다.</div>;
   }
 
-
   return (
     <div className={S.layout}>
+      <ChattingGroupHeader showBackButton={false} coinData={currentCoin}/>
       <Chart
           data={data}
           fetchNextPage={fetchNextPage}
           hasNextPage={hasNextPage}
           isFetchingNextPage={isFetchingNextPage}
           params={chartParams}
-            />
+      />
       <div className={S.contentWrapper}>
         <Outlet context={{ coin: currentCoin }} /> 
+      </div>
+      <div>
+        <ChatInput />
       </div>
     </div>
   );
