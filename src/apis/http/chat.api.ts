c499765ef2eaf2076a208a75576dd72081149760
진준호 <161407999+junhocode@users.noreply.github.com>
2025-12-03 14:35:26 +0900

    refactor: add cursor configs of getChats

--- src/apis/http/chat.api.ts
+++ src/apis/http/chat.api.ts
@@ -2,9 +2,20 @@ import fetcher from '@/apis/fetcher';
 import { BASE_URL, ENDPOINT } from '@/apis/url';
 import type { Chat, ChattingGroupResponse } from '@/types/chat.type';
 
-export const getChats = async (chattingGroupId: number): Promise<ChattingGroupResponse> => {
+export const getChats = async (
+  chattingGroupId: number,
+  cursor: { createdAt: string; chatId: number } = {
+    createdAt: new Date().toISOString(),
+    chatId: 0,
+  },
+  pageSize: number = 20,
+): Promise<ChattingGroupResponse> => {
   return fetcher.get<ChattingGroupResponse>({
     url: BASE_URL + ENDPOINT.CHAT_GROUP(chattingGroupId),
+    params: {
+      cursor: JSON.stringify(cursor),
+      pageSize,
+    },
   });
 };
 
