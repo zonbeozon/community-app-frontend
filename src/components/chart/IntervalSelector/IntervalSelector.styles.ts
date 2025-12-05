f73820699ec25d92124936e8ca1e210029323f96
진준호 <161407999+junhocode@users.noreply.github.com>
2025-12-05 18:20:31 +0900

    feat: add IntervalSelector

---
+++ src/components/chart/IntervalSelector/IntervalSelector.styles.ts
@@ -0,0 +1,4 @@
+export const toggleGroup = "gap-2";
+
+export const toggleGroupItems =
+  "cursor-pointer hover:!bg-transparent text-foreground transition duration-200 ease-in-out hover:text-yellow-600 dark:hover:text-yellow-300 data-[state=on]:!bg-transparent data-[state=on]:text-yellow-600 data-[state=on]:dark:text-yellow-300 data-[state=on]:font-bold";
---
+++ src/components/chart/IntervalSelector/IntervalSelector.tsx
@@ -0,0 +1,34 @@
+import { INTERVALS } from "@/constants/constants";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import type { SelectorProps } from "@/types/chart.type";
+import * as S from "./IntervalSelector.styles";
+
+export const IntervalSelector = ({ value, onChange }: SelectorProps) => {
+  const handleValueChange = (newValue: string) => {
+    if (newValue) {
+      onChange(newValue);
+    }
+  };
+
+  return (
+    <div>
+      <ToggleGroup
+        type="single"
+        value={value}
+        onValueChange={handleValueChange}
+        className={S.toggleGroup}
+      >
+        {INTERVALS.map((interval) => (
+          <ToggleGroupItem
+            key={interval}
+            value={interval}
+            aria-label={`Select ${interval}`}
+            className={S.toggleGroupItems}
+          >
+            {interval}
+          </ToggleGroupItem>
+        ))}
+      </ToggleGroup>
+    </div>
+  );
+};
