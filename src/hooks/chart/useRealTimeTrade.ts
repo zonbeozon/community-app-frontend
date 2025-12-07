ad991a4960397790169b672640ccc2ce4b1eb3bd
진준호 <161407999+junhocode@users.noreply.github.com>
2025-12-07 12:21:23 +0900

    feat: add displayPrice in useRealTimeTrade

--- src/hooks/chart/useRealTimeTrade.ts
+++ src/hooks/chart/useRealTimeTrade.ts
@@ -1,22 +1,41 @@
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import useWebSocket from 'react-use-websocket';
 
 const WS_BASE_URL = import.meta.env.VITE_BINANCE_WS_URL;
 
 export const useRealTimeTrade = (symbol: string) => {
+  const [displayPrice, setDisplayPrice] = useState<number | null>(null);
+  
   const latestPriceRef = useRef<number | null>(null);
 
-  const wsUrl = symbol ? `${WS_BASE_URL}/${symbol.toLowerCase()}@trade` : null;
+  const wsUrl = symbol 
+    ? `${WS_BASE_URL}/${symbol.toLowerCase()}usdt@trade` 
+    : null;
 
   useWebSocket(wsUrl, {
     onMessage: (event) => {
       const tradeData = JSON.parse(event.data);
       if (tradeData.e === 'trade' && tradeData.p) {
-        latestPriceRef.current = parseFloat(tradeData.p);
+        const price = parseFloat(tradeData.p);
+        latestPriceRef.current = price;
+
+        if (displayPrice === null) {
+          setDisplayPrice(price);
+        }
       }
     },
     shouldReconnect: () => true,
   });
 
-  return latestPriceRef;
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (latestPriceRef.current !== null) {
+        setDisplayPrice(latestPriceRef.current);
+      }
+    }, 15000); 
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return displayPrice;
 };
